a03b3d572aac3e99df203a485548ee2cb831dea0
Jessica Peterson <jpeterson8@zagmail.gonzaga.edu>
2019-09-05 16:21:11 -0700

    Initial Commit

---
+++ The Awesome Ones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Awesome Ones
+//
+//  Created by Jessica Peterson on 9/5/19.
+//  Copyright © 2019 Jessica Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
