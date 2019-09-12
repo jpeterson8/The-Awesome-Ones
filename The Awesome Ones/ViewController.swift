d830a9c94c4e69ac3d890c4d88e73ec27e40e401
Jessica Peterson <jpeterson8@zagmail.gonzaga.edu>
2019-09-11 17:59:54 -0700

    test commit to see if anything is wrong with remote

--- The Awesome Ones/ViewController.swift
+++ The Awesome Ones/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewer: UIImageView!
+    @IBOutlet weak var label: UILabel! //names
+    
+    var index = -1
+    var newIndex = -1
+    var randomStart = Int.random(in: 0...8)
+    
+    var people = ["Ethan",
+                  "Braden",
+                  "Kyle",
+                  "Reece",
+                  "Max",
+                  "Jessica",
+                  "Sophia",
+                  "Jared"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Button(_ sender: UIButton) {
+    
+    
+        if randomStart > people.count - 1 {
+            randomStart = 0
+        }
+        label.text = people[randomStart]
+        viewer.image = UIImage(named: "\(randomStart)")
+        randomStart += 1
+    }
+    
 }
 
